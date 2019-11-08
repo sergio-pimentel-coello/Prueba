9495f2e5d565655ea7eef03f42b5b52a22a57e1f
Sergio Pimentel Coello <serpimco@hotmail.com>
2019-11-08 12:54:59 +0100

    Commit inicial

---
+++ suma_de_dos_numeros.cpp
@@ -0,0 +1,17 @@
+/*Suma de dos numeros*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numero1, numero2, resultado;
+    
+    cout << "Introduce el primer número: ";
+    cin >> numero1;
+    cout << "Introduce el segundo número: ";
+    cin >> numero2;
+
+    resultado= numero1 + numero2;
+
+    cout << "El resultado de la suma es: " << resultado << endl;
+
+}
